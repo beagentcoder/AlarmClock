7b510d3b3e909659a0cc02029a05da203af2fed3
beagentcoder <sameeryadav1226@gmail.com>
2024-06-25 16:53:42 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,54 @@
+const startBtn = document.getElementById("start");
+const stopBtn = document.getElementById("stop");
+
+const timeEl = document.getElementById("currentTime");
+
+var alarmSound = new Audio("./assets/alarm_wav.mp3");
+let alarmTime = null;
+
+function currentTime() {
+  timeEl.textContent = ``;
+  const date = new Date();
+  const hours = date.getHours();
+  let minutes = date.getMinutes();
+  let seconds = date.getSeconds();
+  const ampm = hours >= 12 ? "PM" : "AM";
+  let hour = hours % 12;
+  hour = hour ? hour : 12;
+
+  minutes = minutes < 10 ? "0" + minutes : minutes;
+  seconds = seconds < 10 ? "0" + seconds : seconds;
+
+  let displayTime = `${hour}:${minutes}:${seconds} ${ampm}`;
+  let now = `${hours}:${minutes}:${seconds}`;
+
+  const timeNow = document.createElement("div");
+  timeNow.id = "currentTime";
+  timeNow.textContent = displayTime;
+  timeEl.appendChild(timeNow);
+  checkAlarm(now);
+  setInterval(currentTime, 1000);
+}
+
+function setAlarm() {
+  var alarmInput = document.getElementById("alarmTime").value;
+  if (alarmInput) {
+    alarmTime = alarmInput + ":" + 0 + 0;
+    console.log("Setting Alarm");
+  }
+}
+function checkAlarm(now) {
+  if (now === alarmTime) {
+    alarmSound.play();
+    console.log("Alarm ringing!");
+    alarmTime = null;
+  }
+}
+function stopAlarm() {
+  alarmSound.pause()
+  alarmTime = null;
+  console.log("Alarm silence!");
+  document.getElementById("alarmTime").value = "";
+}
+
+currentTime();
